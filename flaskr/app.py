2dca2cb0face90cfa9e209edd2e46ac8ddb9496a
Shivansh Sharma <shivanshddlj@gmail.com>
2022-10-12 09:56:19 +0100

    added post requests

--- flaskr/app.py
+++ flaskr/app.py
@@ -3,5 +3,11 @@ import datetime
 import os
 import pprint
 
-import backend
+#import backend
 from flask import Flask, make_response, render_template, request
+
+@app.route("/",methods = ["GET","POST"])
+def route():
+    if request.method == "POST":
+        print(request.form.get("col"))
+    return render_template("index.html")
